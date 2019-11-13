0c1c970515e02c4f8cf9ed3ca32bedf5c191de26
‘Cait <caitleigh27@gmail.com>
2019-11-12 18:02:57 -0800

    linked sales.html and index.html pages, removed h1 elements for each store on sales.html, added a store Name element to each store object on app.js and created an element in app.js for each store to easily add and remove stores as we move forward.

--- app.js
+++ app.js
@@ -1,39 +1,14 @@
+/* eslint-disable no-redeclare */
 'use strict';
 
-var seattleStoreElement = document.getElementById('seattleStore');
-var tokyoStoreElement = document.getElementById('tokyoStore');
-var dubaiStoreElement = document.getElementById('dubaiStore');
-var parisStoreElement = document.getElementById('parisStore');
-var limaStoreElement = document.getElementById('limaStore');
-
-// var liEl = document.createElement('li');
-
-// liEl.textContent = 'proof of life';
-
-// seattleStoreElement.appendChild(liEl);
+//Global Elements
+var storeInfoElement = document.getElementById('storeInfo');
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-var seattleStoreInfo = {
-  minCustomersEachHour: 23,
-  maxCustomersEachHour: 65,
-  averageCookiesPerCustomer: 6.3,
-  cookiesEachHour: [],
-  totalCookiesForDay: 0,
-
-  render: function() {
-    for (var i=0; i < hours.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = `${hours[i]}: Cookies ${this.cookiesEachHour[i]}`;
-      seattleStoreElement.appendChild(liEl);
-    }
-    var liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.totalCookiesForDay}`; 
-    seattleStoreElement.appendChild(liEl);
-  }
-};
 
 //console.log('cookiesEachHour: ', seattleStoreInfo.cookiesEachHour);
+//funciton below calculates cookies each hour, and pushes that info to the 'cookies each hour' array for each store object. Also calculates total cookies per day and pushes to counter for the 'total cookies per day' element of each store object. Both are rounded to a whole number.
 
 function cookiesEachHourTotal (object) {
   for (var i=0; i < hours.length; i++) {
@@ -44,18 +19,49 @@ function cookiesEachHourTotal (object) {
   }
 }
 
-
+// Function used to calcualte random number between two variables and inclusive of variables and rounds them to a whole number.
 function randomNumber (min , max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-cookiesEachHourTotal (seattleStoreInfo);
-console.log('seattleStoreInfo: ' , seattleStoreInfo);
 
+//Seattle store object
+var seattleStoreInfo = {
+  storeName: 'Seattle',
+  minCustomersEachHour: 23,
+  maxCustomersEachHour: 65,
+  averageCookiesPerCustomer: 6.3,
+  cookiesEachHour: [],
+  totalCookiesForDay: 0,
+
+  render: function() {
+    var h2El = document.createElement('h2');
+    h2El.textContent = this.storeName;
+    storeInfoElement.appendChild(h2El);
+
+    for (var i=0; i < hours.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = `${hours[i]}: Cookies ${this.cookiesEachHour[i]}`;
+      storeInfoElement.appendChild(liEl);
+    }
+    var liEl = document.createElement('li');
+    liEl.textContent = `Total: ${this.totalCookiesForDay}`; 
+    storeInfoElement.appendChild(liEl);
+  }
+};
+
+
+//call cookie total counter
+cookiesEachHourTotal (seattleStoreInfo);
+//console.log('seattleStoreInfo: ' , seattleStoreInfo);
+
+// call seattle store info to display list of hours and cookies per hour
 seattleStoreInfo.render();
 
+//Toyko Store object
 var tokyoStoreInfo = {
+  storeName: 'Tokyo',
   minCustomersEachHour: 3,
   maxCustomersEachHour: 24,
   averageCookiesPerCustomer: 1.2,
@@ -63,22 +69,28 @@ var tokyoStoreInfo = {
   totalCookiesForDay: 0,
 
   render: function() {
+    var h2El = document.createElement('h2');
+    h2El.textContent = this.storeName;
+    storeInfoElement.appendChild(h2El);
+
     for (var i = 0 ; i < hours.length; i++) {
       var liEl = document.createElement('li');
       liEl.textContent = `${hours[i]}: Cookies ${this.cookiesEachHour[i]}`;
-      tokyoStoreElement.appendChild(liEl);
+      storeInfoElement.appendChild(liEl);
     }
+    // eslint-disable-next-line no-redeclare
     var liEl = document.createElement('li');
     liEl.textContent = `Total: ${this.totalCookiesForDay}`; 
-    tokyoStoreElement.appendChild(liEl);
+    storeInfoElement.appendChild(liEl);
   }
 };
 
 cookiesEachHourTotal (tokyoStoreInfo);
 tokyoStoreInfo.render();
 
-
+//Dubai Store object
 var dubaiStoreInfo = {
+  storeName: 'Dubai',
   minCustomersEachHour: 11,
   maxCustomersEachHour: 28,
   averageCookiesPerCustomer: 3.7,
@@ -86,21 +98,28 @@ var dubaiStoreInfo = {
   totalCookiesForDay: 0,
 
   render: function() {
+    var h2El = document.createElement('h2');
+    h2El.textContent = this.storeName;
+    storeInfoElement.appendChild(h2El);
+
     for (var i = 0 ; i < hours.length; i++) {
       var liEl = document.createElement('li');
       liEl.textContent = `${hours[i]}: Cookies ${this.cookiesEachHour[i]}`;
-      dubaiStoreElement.appendChild(liEl);
+      storeInfoElement.appendChild(liEl);
     }
     var liEl = document.createElement('li');
     liEl.textContent = `Total: ${this.totalCookiesForDay}`; 
-    dubaiStoreElement.appendChild(liEl);
+    storeInfoElement.appendChild(liEl);
   }
 };
 
 cookiesEachHourTotal (dubaiStoreInfo);
 dubaiStoreInfo.render ();
 
+
+//Paris Store obejct
 var parisStoreInfo = {
+  storeName: 'Paris',
   minCustomersEachHour: 20,
   maxCustomersEachHour: 38,
   averageCookiesPerCustomer: 2.3,
@@ -108,21 +127,27 @@ var parisStoreInfo = {
   totalCookiesForDay: 0,
 
   render: function() {
+    var h2El = document.createElement('h2');
+    h2El.textContent = this.storeName;
+    storeInfoElement.appendChild(h2El);
     for (var i = 0 ; i < hours.length; i++) {
       var liEl = document.createElement('li');
       liEl.textContent = `${hours[i]}: Cookies ${this.cookiesEachHour[i]}`;
-      parisStoreElement.appendChild(liEl);
+      storeInfoElement.appendChild(liEl);
     }
     var liEl = document.createElement('li');
     liEl.textContent = `Total: ${this.totalCookiesForDay}`; 
-    parisStoreElement.appendChild(liEl);
+    storeInfoElement.appendChild(liEl);
   }
 };
 
 cookiesEachHourTotal (parisStoreInfo);
 parisStoreInfo.render ();
 
+
+//Lima Store object
 var limaStoreInfo = {
+  storeName: 'Lima',
   minCustomersEachHour: 2,
   maxCustomersEachHour: 16,
   averageCookiesPerCustomer: 4.6,
@@ -130,14 +155,17 @@ var limaStoreInfo = {
   totalCookiesForDay: 0,
 
   render: function() {
+    var h2El = document.createElement('h2');
+    h2El.textContent = this.storeName;
+    storeInfoElement.appendChild(h2El);
     for (var i = 0 ; i < hours.length; i++) {
       var liEl = document.createElement('li');
       liEl.textContent = `${hours[i]}: Cookies ${this.cookiesEachHour[i]}`;
-      limaStoreElement.appendChild(liEl);
+      storeInfoElement.appendChild(liEl);
     }
     var liEl = document.createElement('li');
     liEl.textContent = `Total: ${this.totalCookiesForDay}`; 
-    limaStoreElement.appendChild(liEl);
+    storeInfoElement.appendChild(liEl);
   }
 };
 
